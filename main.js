c316741af25957b4720dbd55c590ce92cfb47e68
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-05-30 13:29:13 +0530

    Add audio

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let servers = {
 let init = async () => {
 
 
-    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
 
 
     document.getElementById('user-1').srcObject = localStream
